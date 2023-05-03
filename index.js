43dea7521f9ac065fb3ebf097e556b6a19f919a8
kfrFahim <kfrfahim3@gmail.com>
2023-05-03 14:07:46 +0600

    create json data file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.use(cors());
+
+const chefdata = require("./data/chefdata.json")
+
+
+app.get("/" , (req ,  res) =>{
+     res.send("My server is running baby")
+} )
+
+
+
+
+
+app.listen(port , ()=> {
+     console.log(`Server app listening on port ${port}`)
+})
